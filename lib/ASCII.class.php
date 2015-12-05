284e3e616ca9bd675e455e6312e5f76f762b96cf
Jack Szwergold <JackSzwergold@gmail.com>
2015-12-04 20:34:39 -0500

    Small cleanup of odd non-thing.

--- lib/ASCII.class.php
+++ lib/ASCII.class.php
@@ -148,8 +148,7 @@ class imageASCII extends imageMosaic {
   // Render the pixel boxes into a container.
   function render_pixel_box_container ($blocks) {
 
-    $ret = ''
-         . '<div class="PixelBoxContainer">' . "\r\n"
+    $ret = '<div class="PixelBoxContainer">' . "\r\n"
          . '<pre>' . "\r\n"
          . sprintf('%s', implode('', $blocks))
          . '</pre>' . "\r\n"
