9237ad7252e6c158924eedaff71263bb391f5012
Jack Szwergold <JackSzwergold@gmail.com>
2025-12-11 13:33:53 -0500

    More refactoring and cleanup.

--- lib/Mosaic.class.php
+++ lib/Mosaic.class.php
@@ -62,34 +62,46 @@ class imageMosaic {
   public $image_quality = array('gif' => 100, 'jpeg' => 100, 'png' => 9);
   public $cache_expiration_in_minutes = 720;
 
+  //**************************************************************************************//
+  // Set the constructor.
   public function __construct() {
   } // __construct
 
-
+  //**************************************************************************************//
+  // Set the debug mode.
   public function debug_mode($DEBUG_MODE) {
 
     $this->DEBUG_MODE = $DEBUG_MODE;
 
   } // debug_mode
 
-
+  //**************************************************************************************//
+  // The row flip horizontal function.
   public function row_flip_horizontal($row_flip_horizontal) {
     if (version_compare(phpversion(), $this->php_version_imageflip, '>')) {
       $this->row_flip_horizontal = $row_flip_horizontal;
-    }
+    } // if
   } // row_flip_horizontal
 
-
-  public function set_image($image_file, $width_resampled, $height_resampled, $block_size) {
-
-    $this->image_file = $image_file;
-    $this->width_resampled = $width_resampled;
-    $this->height_resampled = $height_resampled;
-    $this->block_size_x = $block_size;
-    $this->block_size_y = $block_size;
-
+  //**************************************************************************************//
+  // The set image function.
+  public function set_image($image_file = null, $width_resampled = null, $height_resampled = null, $block_size = null) {
+    if (!empty($image_file)) {
+      $this->image_file = $image_file;
+    } // if
+    if (!empty($width_resampled)) {
+      $this->width_resampled = $width_resampled;
+    } // if
+    if (!empty($height_resampled)) {
+      $this->height_resampled = $height_resampled;
+    } // if
+    if (!empty($block_size)) {
+      $this->block_size_x = $block_size;
+    } // if
+    if (!empty($block_size)) {
+      $this->block_size_y = $block_size;
+    } // if
   } // set_image
-
 
   // Set the generate images value.
   function set_generate_images ($generate_images = null) {
